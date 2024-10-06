18b7ab48f65c9f19fe1794a9d04b7a44406f768b
Your Name <your_email@example.com>
2024-10-06 19:21:49 +0300

    Initial commit for anniversary website

---
+++ home.js
@@ -0,0 +1,5 @@
+// Optional: You can add more interactivity here if you'd like
+// For example, when the user clicks the button, you can trigger a confetti effect
+document.querySelector('.btn').addEventListener('click', () => {
+    alert('Happy Anniversary! ❤️');
+});
